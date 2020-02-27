65a797c43c372dbf8a4cee3d3db82c72e09fe429
Suvi <suvi.rannisto@gmail.com>
2020-02-27 15:06:47 +0200

    Ulkoasun ja template struktuurin määritys

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function main() {
-    console.log("Hello");
+    getMeasures();
 }
 
 function fetchResult(endpoint,callback) {
@@ -12,4 +12,12 @@ function fetchResult(endpoint,callback) {
         .then((data) => {
             callback(data);
         });
+}
+
+function getMeasures() {
+    fetchResult('/measures', function (data) {
+        let template = "<p>weight: <b><% this.Weight %></b> <% this.MeasureDate %> </p>";
+        let contents = applyTemplate(template, data[0]);
+        document.getElementById("app").innerHTML = contents;
+    });
 }
