560c278bd0a5fadaeb824fd571362626b37384a0
Suvi <suvi.rannisto@gmail.com>
2020-03-03 13:49:01 +0200

    Http-metodit toimivat selaimesta tietokantaan
    Toteutettu nyt:
    - get('/measures/:userId')
    - post('/measures/:userId')
    - get('/myaccount/:userId')
    - put('/myaccount/:userId')
    - post('/users')

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var connection = mysql.createConnection({
     database: "painonhallinta_db"
 });
 
-
 //public folder as root
 app.use(express.static('wwwroot'));
+app.use(express.json());
 
 connection.connect(function(err) {
     if (err) throw err;
@@ -19,12 +19,71 @@ connection.connect(function(err) {
         res.sendFile('index.html');
     });
 
-    app.get('/measures', function (req, res) {
-        connection.query("SELECT * FROM measures", function (err, result, fields) {
+    //MEASUREMENTS
+    app.get('/measures/:userId', function (req, res) { //Hae käyttäjän 1 mittaukset
+        var userId = req.params.userId;
+        console.log(userId);
+        connection.query("SELECT * FROM measures WHERE UserId = ?", [userId], function (err, result, fields) {
             if (err) throw err;
-            qResult = result;
             res.send(result);
         });
+    });
+    //https://stackoverflow.com/questions/37763764/set-id-in-put-method-nodejs
+    app.post('/measures/:userId', function (req, res) { //Lisää uusi mittaus käyttäjälle 1
+        var userId = req.params.userId;
+        var d = new Date();
+        var todayDate = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDay() //DATE '2020-02-27'
+        console.log("Päivä" + todayDate);
+        var weightToday = req.body.weightToday;
+        console.log("wei" + weightToday);
+        var userId = req.params.userId;
+        connection.query("INSERT INTO measures (MeasureDate,Weight,UserId) VALUES (DATE ?,?,?);",
+            [todayDate, weightToday, userId] , function (err, result, fields) {
+            if (err) throw err;
+            res.send();
+            console.log("Measurement was inserted: " + result);
+        });
+    });
+
+    //ACCOUNT
+    app.get('/myaccount/:userId', function (req, res) { //Hae käyttäjän 1 mittaukset
+        var userId = req.params.userId;
+        connection.query("SELECT * FROM users WHERE Id = ?", [userId], function (err, result, fields) {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+
+    app.put('/myaccount/:userId', function (req, res) { //Muokkaa käyttäjän 1 tietoja
+        var userId = req.params.userId;
+        var name = req.body.name;
+        var height = req.body.height;
+        var startingWeight = req.body.startingWeight;
+        var targetWeight = req.body.targetWeight;
+        connection.query(
+            "UPDATE users SET UserName = ?, Height = ?, StartingWeight = ?, TargetWeight = ? WHERE Id = ?",
+            [name, height, startingWeight, targetWeight, userId] ,
+            function (err, result, fields) {
+                if (err) throw err;
+                res.send();
+                console.log("myaccount was updated: " + result);
+            });
+
+    });
+
+    app.post('/users', function (req, res) { //Lisää uusi mittaus käyttäjälle 1
+        var name = req.body.name;
+        console.log("NAme" + name);
+        var height = req.body.height;
+        var startingWeight = req.body.startingWeight;
+        var targetWeight = req.body.targetWeight;
+
+        connection.query("INSERT INTO users (UserName, Height, StartingWeight, TargetWeight) VALUES (?,?,?,?);",
+            [name.toString(), height, startingWeight, targetWeight] , function (err, result, fields) {
+                if (err) throw err;
+                res.send();
+                console.log("new account was inserted: " + result);
+            });
     });
 
     var server = app.listen(8081, function () {
--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -2,6 +2,10 @@
 
 function main() {
     getMeasures();
+    postMeasure();
+    getMyAccount();
+    putMyAccount();
+    postNewUser();
 }
 
 function fetchResult(endpoint,callback) {
@@ -15,9 +19,32 @@ function fetchResult(endpoint,callback) {
 }
 
 function getMeasures() {
-    fetchResult('/measures', function (data) {
+    fetchResult('/measures/:userId', function (data) {
         let template = "<p>weight: <b><% this.Weight %></b> <% this.MeasureDate %> </p>";
         let contents = applyTemplate(template, data[0]);
         document.getElementById("app").innerHTML = contents;
+    });
+}
+
+function postMeasure() {
+    fetchResult('/measures/:userId', function (data) {
+    });
+}
+
+function getMyAccount() {
+    fetchResult('/myaccount/:userId', function (data) {
+        let template = "<p>weight: <b><% this.Weight %></b> <% this.MeasureDate %> </p>";
+        let contents = applyTemplate(template, data[0]);
+        document.getElementById("app").innerHTML = contents;
+    });
+}
+
+function putMyAccount() {
+    fetchResult('/myaccount/:userId', function (data) {
+    });
+}
+
+function postNewUser() {
+    fetchResult('/users', function (data) {
     });
 }
