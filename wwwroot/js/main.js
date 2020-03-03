0579ef4bed399426a9df8ab9b8627d7a0555d03d
Suvi <suvi.rannisto@gmail.com>
2020-03-03 13:56:18 +0200

    Mergen jälkiä fiksailtu

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -5,19 +5,21 @@ const router = new Router({
 });
 
 function main() {
-	  router.add('/', () => {
-        showTemplate("main-template", "app", {});
-        //getMeasures();
-	  });
+    router.add('/', () => {
+    showTemplate("main-template", "app", {});
+        /*getMeasures();
+        postMeasure();
+        getMyAccount();
+        putMyAccount();
+        postNewUser();*/
+    });
 
-	  router.add('/login', () => {
+    router.add('/login', () => {
         console.log("hello!");
-	  });
+    });
 
     router.addUriListener();
-
-	  router.navigateTo(window.location.pathname);
-
+    router.navigateTo(window.location.pathname);
     window.router = router;
 }
 
@@ -41,5 +43,28 @@ function getMeasures() {
         let template = "<p>weight: <b><% this.Weight %></b> <% this.MeasureDate %> </p>";
         let contents = applyTemplate(template, data[0]);
         document.getElementById("app").innerHTML = contents;
+    });
+}
+
+function postMeasure() {
+    fetchResult('/measures/:userId', function (data) {
+    });
+}
+
+function getMyAccount() {
+    fetchResult('/myaccount/:userId', function (data) {
+        let template = "<p>weight: <b><% this.Weight %></b> <% this.MeasureDate %> </p>";
+        let contents = applyTemplate(template, data[0]);
+        document.getElementById("app").innerHTML = contents;
+    });
+}
+
+function putMyAccount() {
+    fetchResult('/myaccount/:userId', function (data) {
+    });
+}
+
+function postNewUser() {
+    fetchResult('/users', function (data) {
     });
 }
