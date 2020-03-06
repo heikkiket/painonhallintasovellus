e8b7d460bb18c48ba7b7f9bede1458a9203e7536
Suvi <suvi.rannisto@gmail.com>
2020-03-06 15:13:56 +0200

    getMeasures() verbs.js:ssä toimii ja printtaa tulokset templateen

--- wwwroot/js/verbs.js
+++ wwwroot/js/verbs.js
@@ -1,23 +1,30 @@
 'use strict';
 
-let height = 160;
+let height = 1.60;  //Hae tietokannasta!!
+let startingWeight = 100;   //Hae tietokannasta!!
 
 function getMeasures() {
-    console.log("Sisällä");
     let userId = 2;
     fetchResult('GET','/measures/'+userId,{}, function (data) {
 
         console.log("length" + data.length);
+        console.log("data" + data);
+
+        let prevWeight = startingWeight;
+        let originalPrevWeight = startingWeight;
+        let i;
+
+        for (i = data.length - 1; i >= 0  ; i--) {
+
+            data[i].MeasureDate = (data[i].MeasureDate.substr(0,10)).split("-").reverse().join("-");
+            data[i].WeightLoss = (prevWeight - data[i].Weight).toFixed(1);
+            data[i].TotalWeightLoss = (originalPrevWeight - data[i].Weight).toFixed(1);
+            data[i].BodyIndex = (data[i].Weight / height / height).toFixed(1);
+
+            prevWeight = data[i].Weight;
+        }
+
         console.log(data);
-        let curWeight = data[data.length-1].Weight;
-        let prevWeight = data[data.length-2].Weight;
-        console.log("Weight" + data[0].Weight);
-        let loss = prevWeight - curWeight;
-        let totalLoss = data[0].Weight - curWeight;
-        let bIndex = curWeight / height / height;
-        let dat = {data: data};
-        //let dat = {loss: loss, tLoss: totalLoss, index: bIndex, data: data};
-    console.log(data);
         showTemplate("weight-measures-template","weight-history-place", {data: data });
 
     });
@@ -50,3 +57,4 @@ function postNewUser() {
     fetchResult('POST','/users', {},function (data) {
     });
 }
+
